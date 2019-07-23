54908673062a1731ff8b77e8c5cce5f4b87f6a7f
Amanda da Silva Ito <adsilva32@gmail.com>
2019-07-23 14:26:48 -0300

    EndPoint para listar Enderecos

--- src/main/java/com/banco/apibanco/controller/EnderecoController.java
+++ src/main/java/com/banco/apibanco/controller/EnderecoController.java
@@ -1,10 +1,14 @@
 package com.banco.apibanco.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +19,7 @@ import com.banco.apibanco.model.Endereco;
 import com.banco.apibanco.servico.EnderecoServico;
 
 @RestController
-@RequestMapping("banco/vi/endereco")
+@RequestMapping("banco/v1/endereco")
 public class EnderecoController {
 
 	@Autowired
@@ -26,4 +30,15 @@ public class EnderecoController {
 		enderecoServico.cadastrarEndereco(enderecoDTO);
 		return new ResponseEntity<Endereco>(HttpStatus.CREATED);
 	}
+
+	@GetMapping
+	public ResponseEntity<List<Endereco>> listarEndereco() {
+		List<Endereco> enderecos = enderecoServico.listarEndereco();
+		if (ObjectUtils.isEmpty(enderecos)) {
+			return new ResponseEntity<List<Endereco>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Endereco>>(enderecos, HttpStatus.OK);
+	}
+
+	
 }

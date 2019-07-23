9c0bcbc9b5bb8039da88e328737eac70c4338fc3
Amanda da Silva Ito <adsilva32@gmail.com>
2019-07-23 08:21:58 -0300

    Atualização local

--- src/main/java/com/banco/apibanco/model/Cliente.java
+++ src/main/java/com/banco/apibanco/model/Cliente.java
@@ -12,6 +12,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -19,6 +20,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class Cliente {
 
 	@Id
